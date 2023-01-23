2d712e2508f66a459703d82b9f7c02156e5cdcc2
rajput_asj <am3239@srmist.edu.in>
2023-01-23 23:58:18 +0530

    Finished Project...

---
+++ app/src/main/java/com/example/my_quiz/QuestionAnswer.java
@@ -0,0 +1,33 @@
+package com.example.my_quiz;
+
+public class QuestionAnswer {
+    public static String[] question ={
+            "Which state government has recently launched a scheme named ‘Nadu-Nedu’, for modernising hospitals?",
+            "Which one of the following river flows between Vindhyan and Satpura ranges?",
+            "The Central Rice Research Station is situated in?",
+            "Patanjali is well known for the compilation of?",
+            "Which one of the following rivers originates in Brahmagiri range of Western Ghats?",
+            "Chambal river is a part of?",
+            "Where was the electricity supply first introduced in India"
+    };
+
+    public static String[][] choices = {
+            {"Telangana", "Andhra Pradesh", "Karnataka", "Tamil Nadu"},
+            {"Narmada", "Mahanadi", "Son", "Netravati"},
+            {"Chennai",  "Cuttack", "Bangalore", "Quilon"},
+            {"Yoga Sutra", "Panchatantra", "Brahma Sutra", "Ayurveda"},
+            {" Pennar", "Cauvery", "Krishna", "Tapti"},
+            {" Sabarmati basin", "Ganga basin", "Narmada basin", "Godavari basin"},
+            {"Mumbai", "Dehradun", "Darjeeling", "Chennai"}
+    };
+
+    public static String[] correctAnswers = {
+            "Andhra Pradesh",
+            "Narmada",
+            "Cuttack",
+            "Yoga Sutra",
+            "Cauvery",
+            "Narmada basin",
+            "Darjeeling"
+    };
+}
